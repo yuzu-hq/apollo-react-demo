5bdeec8f77141c42f7fed7c8df65acb2605fcb9e
sdcoffey <stevendcoffey@gmail.com>
2022-09-12 16:02:30 -0700

    simplify the example app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 import  { gql, useQuery } from '@apollo/client'
@@ -24,7 +22,7 @@ const query = gql`
       }
     }
   }
-`
+`;
 
 const defaultSecurities = [
   "AAPL",
@@ -45,85 +43,43 @@ const defaultCryptos = [
   "UNI-USD"
 ]
 
-function keyBy<T>(src: Array<T>, keyer: (item: T) => string): Record<string, T> {
-  return src.reduce<Record<string, T>>((map, item) => {
-    map[keyer(item)] = item;
-    return map;
-  }, {})
-}
-
 function App() {
-  const [securitySymbols, setSecuritySymbols] = useState<string[]>(defaultSecurities)
-  const [cryptoSymbols, setCryptoSymbols] = useState<string[]>(defaultCryptos);
-
   const { data, loading } = useQuery(query, {
     variables: {
-      cryptoSymbols,
-      securitySymbols,
+      cryptoSymbols: defaultCryptos,
+      securitySymbols: defaultSecurities,
     },
-    pollInterval: 5000,
-    skip: !securitySymbols.length && !cryptoSymbols.length,
+    pollInterval: 5000, // Set this to update prices periodically
   })
 
-  const securityData = data ? keyBy(data.securities, (({symbol}) => symbol)) : {};
-  const cryptoData = data ? keyBy(data.cryptoTradingPairs, (({symbol}) => symbol)) : {};
-
-  const toggle = (
-    setter: typeof setSecuritySymbols | typeof setCryptoSymbols,
-    symbols: string[],
-    key: string
-  ) => () => {
-    if (symbols.indexOf(key) < 0) {
-      symbols.push(key);
-    } else {
-      symbols = symbols.filter((s) => s !== key);
-    }
-
-    setter(() => symbols);
+  if (loading) {
+    return "Loading..."
   }
 
-  console.log({securitySymbols, cryptoSymbols});
-
   return (
-    <div className="App">
-      <div className="App__header">
-        🍋 Yuzu React Demo
-      </div>
-      <div className="App__body">
-        {loading && !data && "Loading"}
-        {!loading && (
-          <div className="">
-            <h1>Stocks</h1>
-            <div className="App__chip-container">
-              {defaultSecurities.map((symbol) => {
-                const price = securityData[symbol]?.lastTrade.price
-                return (
-                  <div className={`Chip ${price && 'Chip--active'}`} key={symbol}
-                    onClick={toggle(setSecuritySymbols, securitySymbols, symbol)}>
-                    <p>{symbol} {price && ':'}</p>
-                    {price && (<p>${price}</p>)}
-                  </div>
-                )
-              })}
-            </div>
-            <h1>Crypto</h1>
-            <div className="App__chip-container">
-              {defaultCryptos.map((symbol) => {
-                const price = cryptoData[symbol]?.lastTrade.price;
-                return (
-                  <div className={`Chip ${price && 'Chip--active'}`} key={symbol}
-                    onClick={toggle(setSecuritySymbols, securitySymbols, symbol)}>
-                    <p>{symbol} {price && ':'}</p>
-                    {price && (<p>${price}</p>)}
-                  </div>
-                  )
-              })}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <td>Symbol</td>
+          <td>Current Price</td>
+        </tr>
+      </thead>
+      <tbody>
+        {data.securities.map(({symbol, lastTrade}) => (
+          <tr>
+            <td>{symbol}</td>
+            <td>${lastTrade.price}</td>
+          </tr>
+        ))}
+        {data.cryptoTradingPairs.map(({symbol, lastTrade}) => (
+          <tr>
+            <td>{symbol}</td>
+            <td>${lastTrade.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
-export default App
+export default App 
